bbac48c49bd842cbc26d0e747697a42d7ed3e176
pciruelos <pciruelos@yahoo.com>
2024-04-09 16:26:37 +0200

    init commit

---
+++ js/subtract.js
@@ -0,0 +1,5 @@
+function subtrac(a,b) {
+    return a - b
+}
+
+module.exports = subtrac
---
+++ js/sum.test.js
@@ -0,0 +1,5 @@
+const sum = require('./sum')
+
+test("test correct" , () => {
+    expect(sum(1,2)).toBe(3)
+})
